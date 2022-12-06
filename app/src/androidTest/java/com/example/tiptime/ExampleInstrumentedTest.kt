94e208d0506a5337e925c2dacc1883513dc35c6d
Ati <eskafiati@gmail.com>
2022-12-06 15:22:39 +0330

    instrumented tests done

--- app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.example.tiptime
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.containsString
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,6 +23,50 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+class CalculatorTests {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_20_percent_tip() {
+        // text is entered to the text field and the keyboard got closed
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        // click the Calculate button
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        // asserting the correct tip is displayed
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$10.00"))))
+
+    }
+
+    @Test
+    fun calculate_18_percent_tip() {
+        // text is entered to the text field and the keyboard got closed
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        // click on 18 percent radio button
+        onView(withId(R.id.option_eighteen_percent))
+            .perform(click())
+
+        // click the Calculate button
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        // asserting the correct tip is displayed
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.00"))))
+
+    }
+
+}
+/*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
@@ -21,4 +74,5 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.tiptime", appContext.packageName)
     }
-}
+}*/
+
